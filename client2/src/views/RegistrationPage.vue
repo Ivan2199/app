cde366cfd07e9ac743a33f28c026b6c861ae83b7
Ivan2199 <jankovic2107@gmail.com>
2022-06-23 18:04:20 +0200

    ..

--- client2/src/views/RegistrationPage.vue
+++ client2/src/views/RegistrationPage.vue
@@ -1,142 +1,43 @@
 <template>
-  <v-app id="inspire">
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="8">
-            <v-card class="elevation-12">
-              <v-window v-model="step">
-                <v-window-item :value="1">
-                  <v-row>
-                    <v-col cols="12" md="8">
-                      <v-card-text class="mt-12">
-                        <h1
-                          class="
-                            text-center
-                            display-2
-                            teal--text
-                            text--accent-3
-                          "
-                        >
-                          Sign in to Driving Education
-                        </h1>
-                        <h4 class="text-center mt-4">
-                          Enter your email for registration
-                        </h4>
-                        <v-form>
-                          <v-text-field
-                            label="Email"
-                            name="Email"
-                            prepend-icon="email"
-                            type="text"
-                            color="teal accent-3"
-                          />
-
-                          <v-text-field
-                            id="password"
-                            label="Password"
-                            name="password"
-                            prepend-icon="lock"
-                            type="password"
-                            color="teal accent-3"
-                          />
-                        </v-form>
-                        <h3 class="text-center mt-4">Forgot your password ?</h3>
-                      </v-card-text>
-                      <div class="text-center mt-3">
-                        <v-btn rounded color="teal accent-3" dark
-                          >SIGN IN</v-btn
-                        >
-                      </div>
-                    </v-col>
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">Hello, Friend!</h1>
-                        <h5 class="text-center">
-                          Enter your personal details and start journay with us
-                        </h5>
-                      </v-card-text>
-                      <div class="text-center">
-                        <v-btn rounded outlined dark @click="step++"
-                          >SIGN UP</v-btn
-                        >
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-window-item>
-                <v-window-item :value="2">
-                  <v-row class="fill-height">
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">Welcome Back!</h1>
-                        <h5 class="text-center">
-                          To Keep connected with us please login with your
-                          personnel info
-                        </h5>
-                      </v-card-text>
-                      <div class="text-center">
-                        <v-btn rounded outlined dark @click="step--"
-                          >Sign in</v-btn
-                        >
-                      </div>
-                    </v-col>
-
-                    <v-col cols="12" md="8">
-                      <v-card-text class="mt-12">
-                        <h1
-                          class="
-                            text-center
-                            display-2
-                            teal--text
-                            text--accent-3
-                          "
-                        >
-                          Create Account
-                        </h1>
-                        <h4 class="text-center mt-4">
-                          Ensure your email for registration
-                        </h4>
-                        <v-form>
-                          <v-text-field
-                            label="Name"
-                            name="Name"
-                            prepend-icon="person"
-                            type="text"
-                            color="teal accent-3"
-                          />
-                          <v-text-field
-                            label="Email"
-                            name="Email"
-                            prepend-icon="email"
-                            type="text"
-                            color="teal accent-3"
-                          />
-
-                          <v-text-field
-                            id="password"
-                            label="Password"
-                            name="password"
-                            prepend-icon="lock"
-                            type="password"
-                            color="teal accent-3"
-                          />
-                        </v-form>
-                      </v-card-text>
-                      <div class="text-center mt-n5">
-                        <v-btn rounded color="teal accent-3" dark
-                          >SIGN UP</v-btn
-                        >
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-window-item>
-              </v-window>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-  </v-app>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-card class="elevation-12">
+        <v-toolbar dark color="primary">
+          <v-toolbar-title>Signup form</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              prepend-icon="person"
+              v-model="name"
+              label="Name"
+              type="text"
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="person"
+              v-model="email"
+              label="Email"
+              type="email"
+            ></v-text-field>
+            <v-text-field
+              id="password"
+              prepend-icon="lock"
+              v-model="password"
+              label="Password"
+              type="password"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" type="submit" @click="registerUser"
+            >Signup</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
